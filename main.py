f9cb5142afce4fdb639ef4f6ad6f01bbcd2b2447
Jared Pekar <jpekar@hrg.lab>
2018-04-12 10:04:33 -0500

    Added a 2 element array, updated main.py, added a linear weight ge py file

--- main.py
+++ main.py
@@ -1,21 +1,49 @@
 import HFSS_Python.HFSSLibrary as hfss
 import numpy as np
+import sys, os
+file_dir = os.path.dirname('linear_weight_gen.py')
+sys.path.append(file_dir)
+import linear_weight_gen as wg
 
-oAnsys,oDesktop = hfss.openHFSS()
-print(oDesktop)
+oAnsys, oDesktop = hfss.openHFSS()
+print(oDesktop.GetActiveProject())
 test = oDesktop.GetActiveProject().GetName()
 print(test)
 
 if not test == 'dip_tut':
     oDesktop.SetActiveProject('dip_tut.aedt')
 
-excitations = ['1']
-magnitudes = [3]
-modes = np.ones((1,len(magnitudes)))
-phases = [45]
+def main():
+
+    n = 2  # Antennas
+    d = 0.5  # Spacing in wavelengths
+    soi = 20  # Signals of Interest locations
+    snoi = 70  # Null location
+
+    exc, phas = wg.weight_gen(n, d, soi, snoi)
+
+    print('exc =', exc)
+    print('phase =', phas)
+
+    ###
+    wg.weight_plot(n, d, soi, snoi)
+    ###
+
+    # excitations = ['1','2']
+    # magnitudes = [3,3]
+    # phases = [30, 40]
+    excitations = ['1','2']
+    magnitudes = exc
+    phases = phas
+
+    modes = np.ones((1, len(magnitudes)))
 
 
 
-oProject = oDesktop.GetActiveProject()
-oDesign = oProject.SetActiveDesign('HFSSDesign1')
-hfss.edit_sources(oDesign, excitations, modes,magnitudes,phases,'W','deg' )
+    oProject = oDesktop.GetActiveProject()
+    oDesign = oProject.SetActiveDesign('HFSSDesign1')
+    hfss.edit_sources(oDesign, excitations, modes,magnitudes,phases,'W','deg' )
+
+    return()
+
+if __name__ == "__main__": main()
